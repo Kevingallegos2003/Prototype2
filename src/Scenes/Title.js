7d8cb86fac79fae569f03131fdee1fcd301f0e7d
Kevin Gallegos <kevingallegos2003@gmail.com>
2024-10-28 13:29:18 -0700

    kevin branch

--- src/Scenes/Title.js
+++ src/Scenes/Title.js
@@ -1,9 +1,18 @@
 class Title extends Phaser.Scene {
     constructor() {
         super("TitleScene");
+        this.my = {sprite: {}};
+    }
     preload(){
+        this.load.setPath("./assets/");
+        this.load.image("guitar", "thumbnaimhaha.png");
+        this.load.image("food", "ChocolateChip_360x.jpg");
     }
     create(){
+        let my = this.my;
+        my.sprite.guitar = this.add.sprite(500, 500, "guitar");
+        my.sprite.food = this.add.sprite(590, 50, "food");
+        my.sprite.guitar.setScale(.1);
     }
     update(){
     }
