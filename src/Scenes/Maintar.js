9f0a0364abe5ccfc3c229b3996fc9f91944d7935
Kevin Gallegos <kevingallegos2003@gmail.com>
2024-11-01 12:16:18 -0700

    Buying/spending mechanic

--- src/Scenes/Maintar.js
+++ src/Scenes/Maintar.js
@@ -3,6 +3,9 @@ class Maintar extends Phaser.Scene {
         super("MainScene");
         this.my = {sprite: {}};
         this.click = false;
+        this.defPosX =[148,1158,1122,148];//default positions of all food
+        this.defPosY  = [518,100,573,77];
+        this.costArray = [30,40,50,60]; //however works on UI and stuff please modify these costs to your liking as these are placeholder
         this.FoodArray = [];
         this.currency = 0;
     }
@@ -12,10 +15,10 @@ class Maintar extends Phaser.Scene {
     create(){
         let my = this.my;
         my.sprite.guitar = this.add.sprite(640, 350, "guitarDefault").setInteractive();
-        my.sprite.food = this.add.sprite(148, 518, "jackNcheese").setInteractive({ draggable: true });
-        my.sprite.food2 = this.add.sprite(1158, 100, "pickCereal").setInteractive({ draggable: true });
-        my.sprite.food3 = this.add.sprite(1122, 573, "ampNoodle").setInteractive({ draggable: true });
-        my.sprite.food4 = this.add.sprite(148, 77, "stringhetti").setInteractive({ draggable: true });
+        my.sprite.food = this.add.sprite(this.defPosX[0], this.defPosY[0], "jackNcheese").setInteractive({ draggable: true });
+        my.sprite.food2 = this.add.sprite(this.defPosX[1], this.defPosY[1], "pickCereal").setInteractive({ draggable: true });
+        my.sprite.food3 = this.add.sprite(this.defPosX[2], this.defPosY[2], "ampNoodle").setInteractive({ draggable: true });
+        my.sprite.food4 = this.add.sprite(this.defPosX[3], this.defPosY[3], "stringhetti").setInteractive({ draggable: true });
         this.FoodArray.push(my.sprite.food);
         this.FoodArray.push(my.sprite.food2);
         this.FoodArray.push(my.sprite.food3);
@@ -29,10 +32,13 @@ class Maintar extends Phaser.Scene {
         for(let i = 0; i<this.FoodArray.length;i++){
             this.FoodArray[i].on('drag', (pointer, dragX, dragY) =>
                 {
+                    if(this.costArray[i] <= this.currency){//only allowed to drag food if u have enough currency
+
                     this.click = true;
                     this.FoodArray[i].x = dragX;
                     this.FoodArray[i].y = dragY
                     //console.log(this.FoodArray[i].x,this.FoodArray[i].y);
+                    }
                 });
                 this.FoodArray[i].on('pointerup', () =>
                     {
@@ -53,6 +59,10 @@ class Maintar extends Phaser.Scene {
         for(let i = 0; i<this.FoodArray.length;i++){
             if (this.collides(my.sprite.guitar, this.FoodArray[i]) && this.click == false) {//code for what to do when collides goes here
                 console.log("this collides");
+                this.currency -= this.costArray[i];
+                this.FoodArray[i].x = this.defPosX[i];
+                this.FoodArray[i]. y= this.defPosY[i];
+                console.log("money left: ",this.currency);
             }
         }
     }
